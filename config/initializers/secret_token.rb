92da72d944f59c17e48a868a68da79a50b204b75
Iain Murray <murray.iain@gmail.com>
2014-03-21 16:34:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hrconsult::Application.config.secret_key_base = '552d539cb77c6f84ae4954eb243cad001aaecfce9e4be423fb0d32f1f5b0d37bc94bd090ac92f3c0192ca4cd4f60004947824227f774abfe417e5d89066ebd7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hrconsult::Application.config.session_store :cookie_store, key: '_hrconsult_session'
